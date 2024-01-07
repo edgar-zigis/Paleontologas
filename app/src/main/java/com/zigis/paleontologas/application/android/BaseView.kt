252d69b12e2acb1f3210a0f785726107c9693636
Edgar Žigis <tranced.freak@gmail.com>
2024-01-07 22:39:19 +0200

    Get rid of Kotlin synthetics

--- app/src/main/java/com/zigis/paleontologas/application/android/BaseView.kt
+++ app/src/main/java/com/zigis/paleontologas/application/android/BaseView.kt
@@ -1,26 +1,33 @@
 package com.zigis.paleontologas.application.android
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import androidx.core.content.ContextCompat
+import androidx.viewbinding.ViewBinding
 import com.zigis.paleontologas.R
 import com.zigis.paleontologas.application.extensions.setDebounceClickListener
-import kotlinx.android.synthetic.main.toolbar.view.*
-import kotlinx.android.synthetic.main.view_base.view.*
+import com.zigis.paleontologas.databinding.ViewBaseBinding
 
-abstract class BaseView(context: Context, layoutResId: Int) : RelativeLayout(context) {
+abstract class BaseView<V: ViewBinding>(context: Context, val viewBinding: V) : RelativeLayout(context) {
+
+    protected abstract val titleTextResId: Int
+    protected val layoutInflater: LayoutInflater = LayoutInflater.from(context)
 
     var onBack: (() -> Unit)? = null
+
+    private val baseBinding = ViewBaseBinding.inflate(layoutInflater, this)
 
     init {
         isClickable = true
         isFocusable = true
         background = ContextCompat.getDrawable(context, R.drawable.bg_launcher)
 
-        inflate(context, R.layout.view_base, this)
-        inflate(context, layoutResId, viewContainer)
+        addView(baseBinding.root)
+        baseBinding.viewContainer.addView(viewBinding.root)
 
-        backButton.setDebounceClickListener {
+        baseBinding.title.text = context.getString(titleTextResId)
+        baseBinding.backButton.setDebounceClickListener {
             onBack?.invoke()
         }
 
