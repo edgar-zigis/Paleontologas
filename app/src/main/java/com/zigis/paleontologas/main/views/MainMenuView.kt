5fe33842528a0f5f2c904470ed45f072b2e916a9
Edgar Žigis <tranced.freak@gmail.com>
2024-01-15 10:46:16 +0200

    Migrate MainMenuView to ViewBinding (#12)

--- app/src/main/java/com/zigis/paleontologas/main/views/MainMenuView.kt
+++ app/src/main/java/com/zigis/paleontologas/main/views/MainMenuView.kt
@@ -1,6 +1,7 @@
 package com.zigis.paleontologas.main.views
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
@@ -10,9 +11,9 @@ import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nightonke.boommenu.BoomButtons.SimpleCircleButton
 import com.zigis.paleontologas.R
+import com.zigis.paleontologas.databinding.ViewMainMenuBinding
 import com.zigis.paleontologas.periods.adapters.PeriodListAdapter
 import com.zigis.paleontologas.periods.data.entities.Period
-import kotlinx.android.synthetic.main.view_main_menu.view.*
 
 interface MainMenuViewDelegate {
     fun openLanguages()
@@ -25,6 +26,8 @@ class MainMenuView(context: Context) : FrameLayout(context) {
 
     var delegate: MainMenuViewDelegate? = null
 
+    private val binding = ViewMainMenuBinding.inflate(LayoutInflater.from(context))
+
     private val adapter = PeriodListAdapter(emptyList()) {
         delegate?.openPeriod(it)
     }
@@ -32,12 +35,12 @@ class MainMenuView(context: Context) : FrameLayout(context) {
     private var animateLayoutChanges = true
 
     init {
-        inflate(context, R.layout.view_main_menu, this)
         initMenu()
         initPeriodList()
+        addView(binding.root)
     }
 
-    private fun initMenu() {
+    private fun initMenu() = with(binding) {
         for (i in 0 until menuButton.piecePlaceEnum.pieceNumber()) {
             val image = when (i) {
                 0 -> R.drawable.menu_time_scale
@@ -61,7 +64,7 @@ class MainMenuView(context: Context) : FrameLayout(context) {
         }
     }
 
-    private fun initPeriodList() {
+    private fun initPeriodList() = with(binding) {
         periodList.adapter = adapter
         periodList.layoutManager = LinearLayoutManager(context)
         periodList.addItemDecoration(DividerItemDecoration(context, VERTICAL).also {
@@ -69,7 +72,7 @@ class MainMenuView(context: Context) : FrameLayout(context) {
         })
     }
 
-    fun setPeriods(items: List<Period>) {
+    fun setPeriods(items: List<Period>) = with(binding) {
         if (animateLayoutChanges) {
             periodList.layoutAnimation =
                 AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation_fall_down)
