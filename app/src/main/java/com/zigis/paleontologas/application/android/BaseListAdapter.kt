5707e5793fcc6a1bf3ae8201d0ad6e19ac7909f6
Edgar Žigis <tranced.freak@gmail.com>
2024-01-15 10:48:52 +0200

    Migrate BaseListAdapter to support ViewBinding (#13)

--- app/src/main/java/com/zigis/paleontologas/application/android/BaseListAdapter.kt
+++ app/src/main/java/com/zigis/paleontologas/application/android/BaseListAdapter.kt
@@ -1,19 +1,37 @@
 package com.zigis.paleontologas.application.android
 
-import android.view.View
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseListAdapter<T>(
-    open val items: List<T>
-) : RecyclerView.Adapter<BaseListAdapter<T>.ViewHolder>() {
+abstract class BaseListAdapter<T, V : ViewBinding>(
+    open var items: List<T>
+) : RecyclerView.Adapter<BaseListAdapter.ViewHolder>() {
 
-    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    protected lateinit var context: Context
 
-    abstract fun onBindViewHolder(view: View, item: T)
+    class ViewHolder(val binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    abstract fun onBindViewHolder(binding: V, item: T, position: Int)
+    abstract fun onBindingRequested(inflater: LayoutInflater, parent: ViewGroup, viewType: Int): V
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(onBindingRequested(inflater, parent, viewType))
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        onBindViewHolder(holder.view, items[position])
+        onBindViewHolder(holder.binding as V, items[position], position)
     }
 
     override fun getItemCount() = items.size
+
+    open fun updateItems(items: List<T>) {
+        if (this.items == items) return
+        this.items = items
+        notifyDataSetChanged()
+    }
 }
