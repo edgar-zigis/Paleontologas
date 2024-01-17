31591b8102dcbb796fad2d83968759888138e769
Edgar Zigis <edgarzigis@MacBook-Pro-X7KQFXCFL6.local>
2024-01-17 12:26:31 +0200

    Migrate PeriodView to ViewBinding

--- app/src/main/java/com/zigis/paleontologas/periods/views/PeriodView.kt
+++ app/src/main/java/com/zigis/paleontologas/periods/views/PeriodView.kt
@@ -2,7 +2,6 @@ package com.zigis.paleontologas.periods.views
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import com.zigis.paleontologas.R
 import com.zigis.paleontologas.application.android.BaseView
@@ -10,48 +9,63 @@ import com.zigis.paleontologas.application.extensions.dp
 import com.zigis.paleontologas.application.extensions.getDrawable
 import com.zigis.paleontologas.application.extensions.getString
 import com.zigis.paleontologas.application.ui.recyclerview.GridSpacingItemDecoration
+import com.zigis.paleontologas.databinding.ViewParallaxFragmentBinding
+import com.zigis.paleontologas.databinding.ViewParallaxHeaderBinding
+import com.zigis.paleontologas.databinding.ViewPeriodContentBinding
 import com.zigis.paleontologas.periods.adapters.LifeFormListAdapter
 import com.zigis.paleontologas.periods.data.entities.LifeForm
 import com.zigis.paleontologas.periods.data.entities.Period
-import kotlinx.android.synthetic.main.view_parallax_fragment.view.*
-import kotlinx.android.synthetic.main.view_period_content.view.*
-import kotlinx.android.synthetic.main.view_parallax_header.view.*
-import kotlinx.android.synthetic.main.view_period_content.view.title
 import uk.co.senab.photoview.PhotoViewAttacher
 
 interface PeriodViewDelegate {
     fun openLifeForm(lifeForm: LifeForm)
 }
 
-class PeriodView(context: Context) : BaseView(context, R.layout.view_parallax_fragment) {
-
+class PeriodView(context: Context) : BaseView<ViewParallaxFragmentBinding>(
+    context,
+    ViewParallaxFragmentBinding.inflate(LayoutInflater.from(context))
+) {
     companion object {
         private const val lifeFormColumnCount = 2
     }
 
+    override val titleTextResId: Int = R.string.app_name
+
     var delegate: PeriodViewDelegate? = null
 
-    private lateinit var zoomView: View
-    private lateinit var contentView: View
+    private val zoomViewBinding = ViewParallaxHeaderBinding.inflate(LayoutInflater.from(context))
+    private val contentViewBinding = ViewPeriodContentBinding.inflate(LayoutInflater.from(context))
 
-    override fun initialize() {
-        val inflater = LayoutInflater.from(context)
-        zoomView = inflater.inflate(R.layout.view_parallax_header, this, false).also {
-            parallaxScroller.zoomView = it
+    private val adapter = LifeFormListAdapter {
+        delegate?.openLifeForm(it)
+    }
+
+    override fun initialize() = with(viewBinding) {
+        parallaxScroller.zoomView = zoomViewBinding.root
+        parallaxScroller.setScrollContentView(contentViewBinding.root)
+        with(contentViewBinding) {
+            lifeFormList.layoutManager = GridLayoutManager(
+                context,
+                lifeFormColumnCount
+            )
+            lifeFormList.addItemDecoration(
+                GridSpacingItemDecoration(
+                    columns = lifeFormColumnCount,
+                    spacing = dp(6),
+                    includeEdge = false
+                )
+            )
+            lifeFormList.adapter = adapter
         }
-        contentView = inflater.inflate(R.layout.view_period_content, this, false).also {
-            parallaxScroller.setScrollContentView(it)
-        }
-        initLifeFormList()
     }
 
     fun configureWith(period: Period) {
-        title.text = context.getString(period.title)
-
-        zoomView.imageView.setImageDrawable(context.getDrawable(period.artwork))
-        zoomView.photoAuthor.text = period.artworkAuthor
-
-        with(contentView) {
+        setTitle(context.getString(period.title))
+        with(zoomViewBinding) {
+            imageView.setImageDrawable(context.getDrawable(period.artwork))
+            photoAuthor.text = period.artworkAuthor
+        }
+        with(contentViewBinding) {
             title.text = context.getString(period.title)
             timeScale.text = context.getString(R.string.mya, period.timeScale)
             environmentInfo.text = context.getString(period.environmentDescription)
@@ -79,23 +93,6 @@ class PeriodView(context: Context) : BaseView(context, R.layout.view_parallax_fr
     }
 
     fun setLifeForms(items: List<LifeForm>) {
-        lifeFormList.adapter =
-            LifeFormListAdapter(items) {
-                delegate?.openLifeForm(it)
-            }
-    }
-
-    private fun initLifeFormList() {
-        lifeFormList.layoutManager = GridLayoutManager(
-            context,
-            lifeFormColumnCount
-        )
-        lifeFormList.addItemDecoration(
-            GridSpacingItemDecoration(
-                columns = lifeFormColumnCount,
-                spacing = dp(6),
-                includeEdge = false
-            )
-        )
+        adapter.updateItems(items)
     }
 }
