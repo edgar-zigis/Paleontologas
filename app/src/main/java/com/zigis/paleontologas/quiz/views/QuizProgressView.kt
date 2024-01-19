583c3428197c2918bf3e7f2fafb169026f8efe1d
Edgar Žigis <tranced.freak@gmail.com>
2024-01-19 11:11:40 +0200

    Adjust QuizProgress section (#27)

--- app/src/main/java/com/zigis/paleontologas/quiz/views/QuizProgressView.kt
+++ app/src/main/java/com/zigis/paleontologas/quiz/views/QuizProgressView.kt
@@ -1,31 +1,36 @@
 package com.zigis.paleontologas.quiz.views
 
 import android.content.Context
-import android.view.LayoutInflater
 import com.zigis.paleontologas.R
-import com.zigis.paleontologas.application.android.BaseView
+import com.zigis.paleontologas.application.architecture.BaseView
 import com.zigis.paleontologas.application.extensions.setDebounceClickListener
 import com.zigis.paleontologas.databinding.ViewQuizProgressBinding
 
 interface QuizProgressViewDelegate {
     fun onStartQuiz()
+    fun onBackInvoked()
 }
 
-class QuizProgressView(context: Context) : BaseView<ViewQuizProgressBinding>(
-    context,
-    ViewQuizProgressBinding.inflate(LayoutInflater.from(context))
-) {
-    override val titleTextResId: Int = R.string.quiz
+class QuizProgressView(context: Context) : BaseView(context) {
 
     var delegate: QuizProgressViewDelegate? = null
 
-    override fun initialize() = with(viewBinding) {
-        startButton.setDebounceClickListener {
-            delegate?.onStartQuiz()
+    override val binding = ViewQuizProgressBinding.inflate(layoutInflater)
+
+    init {
+        with(binding) {
+            title.text = getString(R.string.quiz)
+            backButton.setDebounceClickListener {
+                delegate?.onBackInvoked()
+            }
+            startButton.setDebounceClickListener {
+                delegate?.onStartQuiz()
+            }
         }
+        addView(binding.root)
     }
 
-    fun setProgress(progress: Float) = with(viewBinding) {
+    fun setProgress(progress: Float) = with(binding) {
         progressBar.setCurrentValues(progress)
     }
 }
