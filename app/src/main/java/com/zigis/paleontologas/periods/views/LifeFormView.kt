9f79dfaa3c91f0f138142cd25b53053903d6b808
Edgar Žigis <tranced.freak@gmail.com>
2024-01-19 11:10:54 +0200

    Adjust LifeFormView  section (#26)

--- app/src/main/java/com/zigis/paleontologas/periods/views/LifeFormView.kt
+++ app/src/main/java/com/zigis/paleontologas/periods/views/LifeFormView.kt
@@ -4,31 +4,45 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import com.zigis.paleontologas.R
-import com.zigis.paleontologas.application.android.BaseView
+import com.zigis.paleontologas.application.architecture.BaseView
 import com.zigis.paleontologas.application.extensions.getDrawable
 import com.zigis.paleontologas.application.extensions.getString
+import com.zigis.paleontologas.application.extensions.setDebounceClickListener
 import com.zigis.paleontologas.databinding.ViewLifeformContentBinding
 import com.zigis.paleontologas.databinding.ViewParallaxFragmentBinding
 import com.zigis.paleontologas.databinding.ViewParallaxHeaderBinding
 import com.zigis.paleontologas.periods.data.entities.LifeForm
 import uk.co.senab.photoview.PhotoViewAttacher
 
-class LifeFormView(context: Context) : BaseView<ViewParallaxFragmentBinding>(
-    context,
-    ViewParallaxFragmentBinding.inflate(LayoutInflater.from(context))
-) {
-    override val titleTextResId: Int = R.string.app_name
+interface LifeFormViewDelegate {
+    fun onBackInvoked()
+}
+
+class LifeFormView(context: Context) : BaseView(context) {
+
+    var delegate: LifeFormViewDelegate? = null
+
+    override val binding = ViewParallaxFragmentBinding.inflate(layoutInflater)
 
     private val zoomViewBinding = ViewParallaxHeaderBinding.inflate(LayoutInflater.from(context))
     private val contentViewBinding = ViewLifeformContentBinding.inflate(LayoutInflater.from(context))
 
-    override fun initialize() = with(viewBinding) {
-        parallaxScroller.zoomView = zoomViewBinding.root
-        parallaxScroller.setScrollContentView(contentViewBinding.root)
+    init {
+        with(binding) {
+            title.text = getString(R.string.about_app)
+            backButton.setDebounceClickListener {
+                delegate?.onBackInvoked()
+            }
+            parallaxScroller.zoomView = zoomViewBinding.root
+            parallaxScroller.setScrollContentView(contentViewBinding.root)
+        }
+        addView(binding.root)
     }
 
     fun configureWith(lifeForm: LifeForm) {
-        setTitle(context.getString(lifeForm.title))
+        with(binding) {
+            title.text = context.getString(lifeForm.title)
+        }
         with(zoomViewBinding) {
             imageView.setImageDrawable(context.getDrawable(lifeForm.artwork))
             photoAuthor.text = lifeForm.artworkAuthor
