54dcdd0825f4345c6a88b7528dc44dc551b1194b
Edgar Zigis <edgarzigis@MacBook-Pro-X7KQFXCFL6.local>
2024-01-21 13:52:23 +0200

    Introduce AndroidLifecycleProvider

--- app/src/main/java/com/zigis/paleontologas/di/applicationModule.kt
+++ app/src/main/java/com/zigis/paleontologas/di/applicationModule.kt
@@ -4,12 +4,14 @@ import com.zigis.paleontologas.core.managers.ApplicationLocaleManager
 import com.zigis.paleontologas.core.managers.ApplicationVersionManager
 import com.zigis.paleontologas.features.launcher.managers.DataMigrationManager
 import com.zigis.paleontologas.core.preferences.ApplicationPreferences
+import com.zigis.paleontologas.core.providers.AndroidLifecycleProvider
 import com.zigis.paleontologas.core.routers.GlobalRouter
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val applicationModule = module {
+    single { AndroidLifecycleProvider(androidContext()) }
     single { ApplicationPreferences(get(named("applicationPreferences"))) }
     single { ApplicationVersionManager(androidContext()) }
     single { ApplicationLocaleManager(get()) }
