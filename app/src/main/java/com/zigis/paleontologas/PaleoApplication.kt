6462d24428734540e4a4f3983a6d719cf90f59fa
Edgar Žigis <tranced.freak@gmail.com>
2024-01-21 13:52:50 +0200

    Introduce AndroidLifecycleProvider (#33)

--- app/src/main/java/com/zigis/paleontologas/PaleoApplication.kt
+++ app/src/main/java/com/zigis/paleontologas/PaleoApplication.kt
@@ -2,12 +2,14 @@ package com.zigis.paleontologas
 
 import android.app.Application
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.zigis.paleontologas.core.providers.AndroidLifecycleProvider
 import com.zigis.paleontologas.di.applicationModule
 import com.zigis.paleontologas.di.dataModule
 import com.zigis.paleontologas.di.preferenceModule
 import com.zigis.paleontologas.di.quizModule
 import com.zigis.paleontologas.di.useCaseModule
 import com.zigis.paleontologas.di.viewModelModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -17,10 +19,13 @@ class PaleoApplication : Application() {
         const val disableCrashLytics = false
     }
 
+    private val androidLifecycleProvider: AndroidLifecycleProvider by inject()
+
     override fun onCreate() {
         super.onCreate()
         startActivityMonitor()
         injectDependencies()
+        registerActivityLifecycleCallbacks(androidLifecycleProvider.activityLifecycleCallbacks)
     }
 
     private fun startActivityMonitor() {
---
+++ app/src/main/java/com/zigis/paleontologas/core/providers/AndroidLifecycleProvider.kt
@@ -0,0 +1,48 @@
+package com.zigis.paleontologas.core.providers
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+
+class AndroidLifecycleProvider constructor(
+    private val applicationContext: Context
+) {
+    val activityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            setActivity(activity)
+        }
+        override fun onActivityStarted(activity: Activity) { setActivity(activity) }
+        override fun onActivityResumed(activity: Activity) { setActivity(activity) }
+        override fun onActivityPaused(activity: Activity) { clearActivity(activity) }
+        override fun onActivityStopped(activity: Activity) { clearActivity(activity) }
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) { }
+        override fun onActivityDestroyed(activity: Activity) { clearActivity(activity) }
+    }
+
+    private var currentActivity: Activity? = null
+
+    fun getActivity(): Activity? {
+        return currentActivity
+    }
+
+    fun getActiveContext(): Context {
+        return getActivity() ?: getApplicationContext()
+    }
+
+    fun getApplicationContext(): Context {
+        return applicationContext
+    }
+
+    private fun setActivity(activity: Activity) {
+        if (currentActivity != activity) {
+            currentActivity = activity
+        }
+    }
+
+    private fun clearActivity(currentActivity: Activity) {
+        if (this.currentActivity == currentActivity) {
+            this.currentActivity = null
+        }
+    }
+}
