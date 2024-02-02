79c4fc9d3ca62d2088cdd9e3cf0f20d2463e80b7
Edgar Žigis <tranced.freak@gmail.com>
2024-02-02 13:53:25 +0200

    Transform LauncherActivity to Compose (#70)

--- features/launcher/src/main/java/com/zigis/paleontologas/features/launcher/LauncherActivity.kt
+++ features/launcher/src/main/java/com/zigis/paleontologas/features/launcher/LauncherActivity.kt
@@ -1,12 +1,31 @@
 package com.zigis.paleontologas.features.launcher
 
 import android.os.Bundle
-import com.zigis.paleontologas.core.architecture.BaseActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.zigis.paleontologas.core.ui.theme.ApplicationTheme
 
-class LauncherActivity : BaseActivity() {
+class LauncherActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launcher)
+
+        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            ApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    //  TODO: Draw launcher content
+                }
+            }
+        }
     }
 }
