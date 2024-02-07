d54e17d5dc20ebaed948a9da43531a29c38501bd
Edgar Žigis <tranced.freak@gmail.com>
2024-02-07 09:31:05 +0200

    Make LanguageFragment compatible with Compose (#76)

---
+++ features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageScreen.kt
@@ -0,0 +1,12 @@
+package com.zigis.paleontologas.features.main.stories.language
+
+import androidx.compose.runtime.Composable
+import org.koin.androidx.compose.koinViewModel
+import java.util.Locale
+
+@Composable
+fun LanguageScreen(
+    viewModel: LanguageViewModel = koinViewModel()
+) {
+
+}
