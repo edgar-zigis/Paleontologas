406e203c19948a59e321fdeeed9381f2572fd796
Edgar Zigis <edgarzigis@MacBook-Pro-X7KQFXCFL6.local>
2024-02-07 09:36:21 +0200

    Move composable to Fragment

--- features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageView.kt
+++ features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageView.kt
@@ -1,47 +1,31 @@
 package com.zigis.paleontologas.features.main.stories.language
 
 import android.content.Context
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zigis.paleontologas.core.architecture.BaseView
-import com.zigis.paleontologas.core.ui.theme.ApplicationTheme
-import com.zigis.paleontologas.features.main.databinding.FragmentComposeBinding
+import com.zigis.paleontologas.core.extensions.setDebounceClickListener
+import com.zigis.paleontologas.features.main.databinding.FragmentLanguageBinding
 import com.zigis.paleontologas.features.main.stories.language.adapter.LanguageListAdapter
 
 class LanguageView(
     context: Context
-) : BaseView<LanguageViewState, FragmentComposeBinding>(context) {
+) : BaseView<LanguageViewState, FragmentLanguageBinding>(context) {
 
     var delegate: LanguageViewDelegate? = null
 
-    override var binding: FragmentComposeBinding? = FragmentComposeBinding.inflate(layoutInflater)
+    override var binding: FragmentLanguageBinding? = FragmentLanguageBinding.inflate(layoutInflater)
 
     private val adapter = LanguageListAdapter {
         delegate?.onLocaleSelected(it)
     }
 
     init {
-        with(requireBinding().rootView) {
-            setViewCompositionStrategy(
-                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
-            )
-            setContent {
-                ApplicationTheme {
-                    Surface(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        LanguageScreen()
-                    }
-                }
-            }
-            /*backButton.setDebounceClickListener {
+        with(requireBinding()) {
+            backButton.setDebounceClickListener {
                 delegate?.onBackInvoked()
             }
             languageList.layoutManager = LinearLayoutManager(context)
-            languageList.adapter = adapter*/
-
+            languageList.adapter = adapter
         }
         addView(requireBinding().root)
     }
