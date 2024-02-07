1f11e7ef821c0c31946ae446fd7807d28bc9a651
Edgar Zigis <edgarzigis@MacBook-Pro-X7KQFXCFL6.local>
2024-02-07 09:51:00 +0200

    Introduce setContent {                 ApplicationTheme {                     Surface(                         modifier = Modifier.fillMaxSize()                     ) {                         LanguageScreen()                     }

--- features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageFragment.kt
+++ features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageFragment.kt
@@ -1,29 +1,16 @@
 package com.zigis.paleontologas.features.main.stories.language
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.Fragment
+import androidx.compose.ui.platform.ComposeView
+import com.zigis.paleontologas.core.architecture.BaseComposableFragment
 import com.zigis.paleontologas.core.ui.theme.ApplicationTheme
-import com.zigis.paleontologas.features.main.databinding.FragmentComposeBinding
 
-class LanguageFragment : Fragment() {
+class LanguageFragment : BaseComposableFragment() {
 
-    private var binding: FragmentComposeBinding? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentComposeBinding.inflate(inflater, container, false)
-        binding?.rootView?.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+    override fun onCreateView(view: ComposeView) {
+        with(view) {
             setContent {
                 ApplicationTheme {
                     Surface(
@@ -34,11 +21,5 @@ class LanguageFragment : Fragment() {
                 }
             }
         }
-        return binding?.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
     }
 }
