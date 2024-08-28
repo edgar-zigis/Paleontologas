05b7eb37e3d73f7cb73c8b9f6f999b1922ef1f56
Edgar Žigis <biomatic@Edgars-MacBook-Air.local>
2024-08-28 13:51:40 +0300

    Migrate Language selection to Jetpack Compose

--- features/main/src/main/java/com/zigis/paleontologas/features/main/stories/language/LanguageViewDelegate.kt
+++
@@ -1,8 +0,0 @@
-package com.zigis.paleontologas.features.main.stories.language
-
-import java.util.Locale
-
-interface LanguageViewDelegate {
-    fun onLocaleSelected(locale: Locale)
-    fun onBackInvoked()
-}
