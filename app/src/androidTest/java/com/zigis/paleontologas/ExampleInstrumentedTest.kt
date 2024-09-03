5d67bef0378e4402cf366806a264493451917be8
Edgar Žigis <edgar.zigis@gmail.com>
2024-09-03 13:37:24 +0300

    Implement QuizProgressScreen using Jetpack Compose (#95)

--- app/src/androidTest/java/com/zigis/paleontologas/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zigis/paleontologas/ExampleInstrumentedTest.kt
@@ -1,24 +1,10 @@
 package com.zigis.paleontologas
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.zigis.paleontologas", appContext.packageName)
-    }
+
 }
