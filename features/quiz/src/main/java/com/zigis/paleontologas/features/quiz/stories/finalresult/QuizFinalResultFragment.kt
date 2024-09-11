1e043e0c1cd85c62ae22f1b82e3a935c57fb5879
Edgar Žigis <edgar.zigis@gmail.com>
2024-09-11 11:27:21 +0300

    Replace QuizFinalResultView with QuizFinalResultScreen (#98)

--- features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/finalresult/QuizFinalResultFragment.kt
+++ features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/finalresult/QuizFinalResultFragment.kt
@@ -1,39 +1,24 @@
 package com.zigis.paleontologas.features.quiz.stories.finalresult
 
-import android.content.Context
-import com.zigis.paleontologas.core.architecture.BaseFragment
-import com.zigis.paleontologas.core.architecture.interfaces.IView
-import com.zigis.paleontologas.core.extensions.sendSafely
-import com.zigis.paleontologas.features.quiz.stories.finalresult.QuizFinalResultIntent.*
-import org.koin.android.ext.android.inject
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.zigis.paleontologas.core.architecture.BaseComposableFragment
+import com.zigis.paleontologas.core.ui.theme.ApplicationTheme
 
-class QuizFinalResultFragment : BaseFragment<QuizFinalResultViewState, QuizFinalResultIntent, QuizFinalResultViewModel>(),
-    QuizFinalResultViewDelegate {
+class QuizFinalResultFragment : BaseComposableFragment() {
 
     var configuration: QuizFinalResultConfiguration? by savedState()
 
-    override val viewModel: QuizFinalResultViewModel by inject()
-
-    override fun onCreateView(context: Context): IView<QuizFinalResultViewState> {
-        return QuizFinalResultView(context).also {
-            it.delegate = this
+    @Composable
+    override fun ViewContentComposition() {
+        ApplicationTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                QuizFinalResultScreen(configuration = configuration!!)
+            }
         }
-    }
-
-    override fun onAttached() {
-        viewModel.intents.sendSafely(Initialize(
-            mark = configuration!!.mark
-        ))
-    }
-
-    override fun onBackPressed(): Boolean {
-        viewModel.intents.sendSafely(InvokeBack)
-        return true
-    }
-
-    //  QuizMarkViewDelegate
-
-    override fun onBackInvoked() {
-        viewModel.intents.sendSafely(InvokeBack)
     }
 }
