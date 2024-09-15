d9c786e426c0a13ce8c2a45be961097235e6cbaf
Edgar Žigis <edgar.zigis@gmail.com>
2024-09-15 17:03:10 +0300

    Implement Success/Failure states in the QuizGameScreen (#100)

--- features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/game/QuizGameViewState.kt
+++ features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/game/QuizGameViewState.kt
@@ -4,5 +4,7 @@ import com.zigis.paleontologas.core.architecture.interfaces.IState
 import com.zigis.paleontologas.features.quiz.data.Question
 
 data class QuizGameViewState(
-    val question: Question? = null
+    val question: Question? = null,
+    val chosenOption: Int? = null,
+    val correctOption: Int? = null
 ) : IState
