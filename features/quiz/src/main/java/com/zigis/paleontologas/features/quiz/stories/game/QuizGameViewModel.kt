a3f60c04c4985a10559222819b8d9e242fe9cbe9
Edgar Žigis <edgar.zigis@gmail.com>
2024-09-15 18:52:28 +0300

    Finalize QuizGameScreen changes (#101)

--- features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/game/QuizGameViewModel.kt
+++ features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/game/QuizGameViewModel.kt
@@ -5,6 +5,7 @@ import com.zigis.paleontologas.features.quiz.data.Question
 import com.zigis.paleontologas.features.quiz.managers.QuizGameProcessor
 import com.zigis.paleontologas.features.quiz.routers.QuizRouter
 import com.zigis.paleontologas.features.quiz.stories.game.QuizGameIntent.*
+import kotlinx.coroutines.delay
 
 class QuizGameViewModel(
     private val quizRouter: QuizRouter,
@@ -33,13 +34,30 @@ class QuizGameViewModel(
     }
 
     private suspend fun answerQuestion(question: Question, option: Int) {
+        updateState {
+            it.copy(
+                chosenOption = option,
+                correctOption = question.getCorrectVariantIndex(),
+                isInTransition = false
+            )
+        }
+        delay(1500L)
+        updateState {
+            it.copy(
+                isInTransition = true
+            )
+        }
+        delay(250L)
         val nextQuestion = quizGameProcessor.answerQuestion(question, option)
         if (nextQuestion == null) {
             quizRouter.openQuizFinalResultPreview(mark = quizGameProcessor.correctAnswers)
         } else {
             updateState {
                 it.copy(
-                    question = nextQuestion
+                    question = nextQuestion,
+                    chosenOption = null,
+                    correctOption = null,
+                    isInTransition = false
                 )
             }
         }
