088eec9f5b56d9d3370fe3afaef98ea19c875653
Edgar Žigis <edgar.zigis@gmail.com>
2025-06-26 11:43:46 +0300

    Implement PlayerAvatar (#207)

---
+++ features/quiz/src/main/java/com/zigis/paleontologas/features/quiz/stories/game/views/PlayerAvatar.kt
@@ -0,0 +1,69 @@
+package com.zigis.paleontologas.features.quiz.stories.game.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zigis.paleontologas.features.quiz.entities.QuizPlayer
+
+@Composable
+fun PlayerAvatar(player: QuizPlayer) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            contentAlignment = Alignment.TopCenter
+        ) {
+            // Circle background
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(color = Color.Gray.copy(alpha = 0.2f), shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = player.avatar,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Text(
+                text = player.country,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.offset(y = 30.dp)
+            )
+        }
+
+        Text(
+            text = player.name,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onBackground,
+            maxLines = 2,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 4.dp, start = 2.dp, end = 2.dp)
+        )
+
+        Text(
+            text = "${player.points} XP",
+            fontSize = 12.sp,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
+        )
+    }
+}
