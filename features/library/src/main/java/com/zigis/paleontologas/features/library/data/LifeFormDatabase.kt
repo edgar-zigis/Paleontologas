39b120efeae88ae88bdd07859c5c5f2d055ea3c4
Edgar Žigis <edgar.zigis@gmail.com>
2025-11-27 20:41:46 +0200

    Fix database migration (#248)

--- features/library/src/main/java/com/zigis/paleontologas/features/library/data/LifeFormDatabase.kt
+++ features/library/src/main/java/com/zigis/paleontologas/features/library/data/LifeFormDatabase.kt
@@ -43,7 +43,7 @@ abstract class LifeFormDatabase : RoomDatabase() {
         val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(db: SupportSQLiteDatabase) {
                 db.execSQL(
-                    "ALTER TABLE life_forms ADD COLUMN `order` INTEGER NOT NULL DEFAULT 0"
+                    "ALTER TABLE life_forms ADD COLUMN orderIndex INTEGER NOT NULL DEFAULT 0"
                 )
             }
         }
